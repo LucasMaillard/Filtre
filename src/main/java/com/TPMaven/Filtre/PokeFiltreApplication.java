14678b6146e86ef618bc48f95c2f968cc1e707ff
Lucas Maillard <lucas.maillard@campus.academy>
2022-01-28 10:17:56 +0100

    Init

---
+++ src/main/java/com/TPMaven/Filtre/PokeFiltreApplication.java
@@ -0,0 +1,13 @@
+package com.TPMaven.Filtre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeFiltreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeFiltreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TPMaven/Filtre/PokeFiltreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPMaven.Filtre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeFiltreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
